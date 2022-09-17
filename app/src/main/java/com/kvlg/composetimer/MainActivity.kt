1b5f98f9054638a02ac781efeb01de5b30d93392
Konstantin Koval <driim@outlook.com>
2022-09-17 20:25:34 +0300

    refactored

--- app/src/main/java/com/kvlg/composetimer/MainActivity.kt
+++ app/src/main/java/com/kvlg/composetimer/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.kvlg.composetimer
 
-import android.graphics.Rect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,17 +11,23 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Refresh
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,12 +39,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,95 +67,118 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview
 fun Timer() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        val angle = remember { Animatable(InitialValue) }
-        var isTutorialVisible by remember { mutableStateOf(false) }
-        var isTimerStarted by remember { mutableStateOf(false) }
-        val borderWidth = with(LocalDensity.current) { 12.dp.toPx() }
-        val textColor = Color.White
-        val textPaint = remember {
-            Paint().asFrameworkPaint().apply {
-                color = textColor.toArgb()
-                textSize = 120.sp.value
-            }
+    var timerValue1 by remember { mutableStateOf(100) }
+    var timerValue2 by remember { mutableStateOf(100) }
+    var isTutorialVisible by remember { mutableStateOf(false) }
+    val angle = remember { Animatable(InitialValue) }
+    var isTimerStarted by remember { mutableStateOf(false) }
+    val borderWidth = with(LocalDensity.current) { 12.dp.toPx() }
+    var isInLeftSide by remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = isTimerStarted) {
+        if (isTimerStarted) {
+            angle.animateTo(targetValue = TargetValue, animationSpec = tween(durationMillis = 5000, easing = LinearEasing))
         }
-        var timerValue1 by remember { mutableStateOf(100) }
-        var timerValue2 by remember { mutableStateOf(100) }
-        val text = "$timerValue1:$timerValue2"
-        val textRect = Rect()
-        textPaint.getTextBounds(text, 0, text.length, textRect)
-        LaunchedEffect(key1 = isTimerStarted) {
-            if (isTimerStarted) {
-                angle.animateTo(targetValue = TargetValue, animationSpec = tween(durationMillis = 5000, easing = LinearEasing))
-            }
-        }
-        val background = MaterialTheme.colorScheme.primary
-        val borderColor = MaterialTheme.colorScheme.secondary
-        Canvas(modifier = Modifier
-            .align(Alignment.Center)
-            .padding(16.dp)
-            .size(256.dp)
-            .pointerInput(key1 = Unit) {
-                var isInRightPart = false
-                detectDragGesturesAfterLongPress(
-                    onDragStart = {
-                        isTutorialVisible = true
-                        isInRightPart = it.x >= size.width / 2
-                    },
-                    onDrag = { input, offset ->
-                        if (isInRightPart) {
-                            timerValue2 -= offset.y.toInt()
-                        } else {
-                            timerValue1 -= offset.y.toInt()
+    }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+        Box(modifier = Modifier.align(Alignment.Center)) {
+            val background = MaterialTheme.colorScheme.primary
+            val borderColor = MaterialTheme.colorScheme.secondary
+            Canvas(modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp)
+                .size(256.dp)
+                .pointerInput(Unit) {
+                    detectDragGesturesAfterLongPress(
+                        onDragStart = {
+                            isTutorialVisible = true
+                            isInLeftSide = it.x <= size.width / 2
+                        },
+                        onDrag = { input, offset ->
+                            if (isInLeftSide) {
+                                timerValue1 -= offset.y.toInt()
+                            } else {
+                                timerValue2 -= offset.y.toInt()
+                            }
+                        },
+                        onDragEnd = {
+                            isTutorialVisible = false
+                        },
+                        onDragCancel = {
+                            isTutorialVisible = false
                         }
-                    },
-                    onDragEnd = {
-                        isTutorialVisible = false
-                    }
-                )
-            }) {
-            drawCircle(color = background)
-            drawArc(color = borderColor,
-                startAngle = StartAngle,
-                sweepAngle = angle.value,
-                useCenter = false,
-                style = Stroke(width = borderWidth, cap = StrokeCap.Round))
-            drawContext.canvas.nativeCanvas.drawText(
-                text,
-                size.width / 2 - textRect.width() / 2,
-                size.height / 2 + textRect.height() / 2,
-                textPaint
+                    )
+                }
+            ) {
+                drawCircle(color = background)
+                drawArc(color = borderColor,
+                    startAngle = StartAngle,
+                    sweepAngle = angle.value,
+                    useCenter = false,
+                    style = Stroke(width = borderWidth, cap = StrokeCap.Round))
+            }
+            TimerText("$timerValue1", "$timerValue2", isTutorial = false)
+        }
+        if (isTimerStarted) {
+            BottomButton(icon = Icons.Rounded.ArrowBack, onClick = { isTimerStarted = !isTimerStarted }, isLeftButton = true)
+        } else {
+            BottomButton(icon = Icons.Rounded.PlayArrow, onClick = { isTimerStarted = !isTimerStarted }, isLeftButton = true)
+        }
+        BottomButton(icon = Icons.Rounded.Refresh, onClick = { isTimerStarted = !isTimerStarted }, isLeftButton = false)
+    }
+    AnimatedVisibility(visible = isTutorialVisible, enter = fadeIn(), exit = fadeOut()) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black.copy(alpha = 0.7f))
+            .padding(16.dp)) {
+            TimerText("$timerValue1", "$timerValue2", isTutorial = true, isLeftSide = isInLeftSide)
+            Text(text = "Minutes", modifier = Modifier.align(Alignment.BottomStart))
+            Text(text = "Seconds", modifier = Modifier.align(Alignment.BottomEnd))
+        }
+    }
+}
+
+@Composable
+fun BoxScope.BottomButton(
+    icon: ImageVector,
+    onClick: () -> Unit,
+    isLeftButton: Boolean,
+) {
+    val indication = rememberRipple(
+        bounded = false,
+        radius = 50.dp
+    )
+    val alignment = if (isLeftButton) Alignment.BottomStart else Alignment.BottomEnd
+    Icon(imageVector = icon,
+        tint = Color.Green,
+        contentDescription = "", modifier = Modifier
+            .padding(start = 32.dp, end = 32.dp)
+            .size(100.dp)
+            .align(alignment)
+            .clickable(interactionSource = remember { MutableInteractionSource() },
+                indication = indication,
+                onClick = onClick
             )
-        }
-        IconButton(onClick = { isTimerStarted = !isTimerStarted }, modifier = Modifier
-            .align(Alignment.BottomStart)
-            .size(120.dp)) {
-            if (isTimerStarted) {
-                Icon(imageVector = Icons.Rounded.ArrowBack,
-                    tint = Color.Green,
-                    contentDescription = "Pause timer", modifier = Modifier.size(120.dp))
-            } else {
-                Icon(imageVector = Icons.Rounded.PlayArrow,
-                    tint = Color.Green,
-                    contentDescription = "Star timer", modifier = Modifier.size(120.dp))
-            }
-        }
-        IconButton(onClick = { }, modifier = Modifier
-            .align(Alignment.BottomEnd)
-            .size(120.dp)) {
-            Icon(imageVector = Icons.Rounded.Refresh,
-                tint = Color.Black,
-                contentDescription = "Reset timer", modifier = Modifier.size(120.dp))
-        }
-        AnimatedVisibility(visible = isTutorialVisible, enter = fadeIn(), exit = fadeOut()) {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Black.copy(alpha = 0.7f))
-                .padding(16.dp)) {
-                Text(text = "Minutes", modifier = Modifier.align(Alignment.BottomStart))
-                Text(text = "Seconds", modifier = Modifier.align(Alignment.BottomEnd))
-            }
-        }
+            .clip(CircleShape)
+            .background(Color.LightGray)
+    )
+}
+
+@Composable
+private fun BoxScope.TimerText(
+    value1: String,
+    value2: String,
+    isTutorial: Boolean = false,
+    isLeftSide: Boolean = true,
+) {
+    val leftTextColor = if (isTutorial && !isLeftSide) Color.Transparent else Color.White
+    val rightTextColor = if (isTutorial && isLeftSide) Color.Transparent else Color.White
+    val colonTextColor = if (isTutorial) Color.Transparent else Color.White
+    Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.align(Alignment.Center)) {
+        Text(text = value1, color = leftTextColor, fontSize = 60.sp)
+        Text(text = ":", color = colonTextColor, fontSize = 60.sp)
+        Text(text = value2, color = rightTextColor, fontSize = 60.sp)
     }
 }
 
